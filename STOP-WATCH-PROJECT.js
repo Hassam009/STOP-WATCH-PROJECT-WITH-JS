20be29c6c8dfd7626474dbfe72d01d301d969ccd
Hassam Amer <hasideveloper@gmail.com>
2024-04-09 11:59:05 +0500

    Finally completed Stop Watch

--- STOP-WATCH-PROJECT.js
+++ STOP-WATCH-PROJECT.js
@@ -1,12 +1,17 @@
+
+
+
+
+
 const countDownElement=document.getElementById("countDown")
 const resetValueElement=document.querySelector(".resetValue")
 
 let startCount=0;
-let intervalId;
+let intervalId; 
 const startTimer=()=>{
     intervalId=  setInterval(()=>{
         countDownElement.innerText=startCount++;
-    },1000)
+    },500)
 }
 document
 .querySelector(".start-btn")
@@ -17,30 +22,34 @@ document
 const stopTimer=()=>{
     clearInterval(intervalId)
 }
-
- 
 document
 .querySelector(".stop-btn")
 .addEventListener("click", stopTimer);
 
+//NOW WILL GIVE FUNCTIONALITIES TO RESET BUTTON
 const resetTimer=()=>{
     startCount=0;
     countDownElement.innerText=startCount;
     clearInterval(intervalId)
 }
-
 document
 .querySelector(".reset-btn")
 .addEventListener("click", resetTimer);
-// document
-// .querySelector(".stop-btn")
-// .addEventListener("click", stopTimer);
-
-// document
-// .querySelector(".time-btn")
-// .addEventListener("click", showStopValue);
 
 
-// document
-// .querySelector(".clear-btn")
-// .addEventListener("click", clearTimeValue)
+//NOW WILL GIVE FUNCTIONALITIES TO SHOW BUTTON
+const showStopValue=()=>{
+    const newPara=document.createElement("p");
+    newPara.innerText=`The Stop Time is ${startCount-1}`
+    resetValueElement.append(newPara);
+}
+document
+.querySelector(".time-btn")
+.addEventListener("click", showStopValue);
+const clearTimeValue=()=>{
+  resetValueElement.innerHTML="";
+}
+
+document
+.querySelector(".clear-btn")
+.addEventListener("click", clearTimeValue)
