d1e300e1b0a3451b5d6a68589c4299d479c0ee89
Hassam Amer <hasideveloper@gmail.com>
2024-04-09 11:40:29 +0500

    Will give functionalities to STOP BUTTON

--- STOP-WATCH-PROJECT.js
+++ STOP-WATCH-PROJECT.js
@@ -1,26 +1,31 @@
 const countDownElement=document.getElementById("countDown")
 const resetValueElement=document.querySelector(".resetValue")
 
-
+let startCount=0;
+let intervalId;
+const startTimer=()=>{
+    intervalId=  setInterval(()=>{
+        countDownElement.innerText=startCount++;
+    },1000)
+}
 document
 .querySelector(".start-btn")
 .addEventListener("click", startTimer);
 
-let startCount=0;
 
-const startTimer=()=>{
-    setInterval(()=>{
-        countDownElement.innerText=startCount++;
-    },1000)
+//NOW WILL GIVE FUNCTIONALITIES TO STOP BUTTON
+const stopTimer=()=>{
+    clearInterval(intervalId)
 }
 
+
+document
+.querySelector(".stop-btn")
+.addEventListener("click", stopTimer);
 
 // document
 // .querySelector(".reset-btn")
 // .addEventListener("click", resetTimer);
-// document
-// .querySelector(".stop-btn")
-// .addEventListener("click", stopTimer);
 // document
 // .querySelector(".stop-btn")
 // .addEventListener("click", stopTimer);
